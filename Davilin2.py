c298805b8e034e410dec0f9cb635a47716eb85c7
SDavila7 <sdavila708@gmail.com>
2024-04-18 11:14:07 -0500

    Mi segundo y tercer commit

---
+++ Davilin2.py
@@ -0,0 +1,24 @@
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para b: "))
+suma=a+b
+print ("La suma es: ",suma)
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para b: "))
+resta=a-b
+print ("La resta es: ",resta)
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para b: "))
+producto=a*b
+print ("El producto de la multiplicación es: ",producto)
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para b: "))
+cociente=a/b
+print ("El cociente de la división es: ",cociente)
+
+a=int(input("Digite el valor para a: "))
+b=int(input("Digite el valor para b: "))
+cocienteR=a%b
+print ("El cociente residuo de la división es: ",cocienteR)
---
+++ Davilin3.py
@@ -0,0 +1,4 @@
+Compra=int(input("¿Cuál fué el valor de la compra? "))
+Pago=int(input("¿Con que billete pagas? "))
+Devueltas=Pago-Compra
+print ("Tus devueltas son: ",Devueltas)
