cfa50d515d5b7586329fe47c114a7cb10b0707df
SDavila7 <sdavila708@gmail.com>
2024-04-18 08:59:11 -0500

    Mi primer commit

---
+++ Davilin.py
@@ -0,0 +1,17 @@
+a=20
+b=5
+
+suma=a+b
+print ("La suma es: ",suma)
+
+resta=a-b
+print ("La resta es: ",resta)
+
+producto=a*b
+print ("El producto de la multiplicación es: ",producto)
+
+cociente=a/b
+print ("El cociente de la división es: ",cociente)
+
+cocienteR=a%b
+print ("El cociente residuo de la división es: ",cocienteR)
