515fea9fcd4919f177f58a3884faafdf9206b263
SDavila7 <167387195+SDavila7@users.noreply.github.com>
2024-04-25 07:12:34 -0500

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,4 +0,0 @@
-nameStudent=(input("¿Cuál es tu nombre?")) #En esta línea se guarda el nombre que el usuario indique con la pregunta generada
-nameCollege=(input("¿En qué institución educativa estudias?")) #En esta línea se guarda el nombre de la institución dónde estudia el usuario
-grade=(input("¿En qué formación estás?")) #En esta línea se guarda la respuesta del usuario a la formación que está realizando (estudio)
-print("El estudiante" ,nameStudent ,"estudia en la instititución llamada", nameCollege,"en la formación de",grade ) #Se muestra todo el mensaje con las respuestas del usuario mostrando el nombre del usuario, de la institución y en qué formación está
